dc7e3dd7f7192c0f7629cac0a90706c0424d1c11
Sad3esuy <nguyenquyngoc2004@gmail.com>
2026-02-02 11:21:00 +0700

    first commit

---
+++ src/main/java/com/example/be_springboot_lum/BeSpringbootLumApplication.java
@@ -0,0 +1,13 @@
+package com.example.be_springboot_lum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeSpringbootLumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeSpringbootLumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/be_springboot_lum/BeSpringbootLumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.be_springboot_lum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeSpringbootLumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
